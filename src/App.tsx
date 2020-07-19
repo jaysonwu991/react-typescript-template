2a34dd8176a51c07995cf6a020785a031ae986b3
Jayson Wu <wulingjie991@outlook.com>
2020-07-19 22:23:09 +0800

    update first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
-import Hello from './components/Hello'
 
 import './App.scss'
 
 const App = () => {
-  return <Hello />
+  return (
+    <div>
+      <div className='information'>
+        {/* <div className='avatar'></div> */}
+        <div className='quote'>Talk Is Cheap, Show Me Your Code.</div>
+      </div>
+      <div className='languages'>
+        <div className='language-item'>Rust</div>
+        <div className='language-item'>Erlang</div>
+        <div className='language-item'>Elixir</div>
+        <div className='language-item'>Ruby</div>
+        <div className='language-item'>C++</div>
+        <div className='language-item'>C</div>
+        <div className='language-item'>Go</div>
+        <div className='language-item'>Python</div>
+        <div className='language-item'>Java</div>
+        <div className='language-item'>PHP</div>
+        <div className='language-item'>Deno</div>
+        <div className='language-item'>Node</div>
+        <div className='language-item'>Dart</div>
+        <div className='language-item'>Typescript</div>
+        <div className='language-item'>Javascript</div>
+        <div className='language-item'>Phoenix</div>
+        <div className='language-item'>Flutter</div>
+        <div className='language-item'>React</div>
+        <div className='language-item'>Vue</div>
+        <div className='language-item'>Angular</div>
+      </div>
+    </div>
+  )
 }
 
 export default App
