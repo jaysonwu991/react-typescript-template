cf6f1c04795a9ee02f9e2bf99f50694e1d42e316
Jayson Wu <wulingjie991@outlook.com>
2021-01-17 10:52:56 +0800

    update packages and format files

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  plugins: [require('autoprefixer')]
-  // plugins: {
-  //   'postcss-import': {},
-  //   'postcss-preset-env': {},
-  //   'postcss-pxtorem': {
-  //     rootValue: 75,
-  //     unitPrecision: 5,
-  //     propWhiteList: [],
-  //     propBlackList: [],
-  //     exclude: false,
-  //     selectorBlackList: [],
-  //     ignoreIdentifier: false,
-  //     replace: true,
-  //     mediaQuery: false,
-  //     minPixelValue: 0
-  //   },
-  //   cssnano: {}
-  // }
+  plugins: {
+    autoprefixer: {}
+    // 'postcss-import': {},
+    // 'postcss-preset-env': {},
+    // 'postcss-pxtorem': {
+    //   rootValue: 75,
+    //   unitPrecision: 5,
+    //   propWhiteList: [],
+    //   propBlackList: [],
+    //   exclude: false,
+    //   selectorBlackList: [],
+    //   ignoreIdentifier: false,
+    //   replace: true,
+    //   mediaQuery: false,
+    //   minPixelValue: 0
+    // },
+    // cssnano: {}
+  }
 }
